4d6c601dcf84c8186c1770c23fa159f225fd8c4c
Tamara <tamara.fendez@gmail.com>
2021-06-11 21:30:49 -0400

    tokens

--- public/desafio-instafake/assets/scripts.js
+++ public/desafio-instafake/assets/scripts.js
@@ -3,8 +3,9 @@ $('#js-form').submit(async (event) => {
     const email = document.getElementById('txtEmail').value;  
     const password = $('#txtPass').val();
     const JWT = await postData(email,password);
+    const album = await getImage(JWT);
 
-    console.log(JWT);
+    console.log(album);
     
 })
 
@@ -25,4 +26,18 @@ const postData = async (email, password) => {
         console.error(`Error: ${err}`);
     }
 }
-// localhost
+
+const getImage = async (token) => {
+    try {
+        const response = await fetch('http://localhost:3000/api/albums', {
+            method:'GET',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const {album} = await response.json()
+        return album
+    } catch (err) {
+        console.error(`Error: ${err}`);
+    }
+}
